9bc0fb94602cb0a3b042815bd0fe9a84435f3c6a
Mathias Gagnon <mathias.gagnon@polymtl.ca>
2024-02-15 22:33:09 -0500

    exemple du cours fonctionne

--- TP2/transform.py
+++ TP2/transform.py
@@ -10,12 +10,12 @@ from numpy import linalg as LA
   V = R − G 
 """
 def ToYUV(data:np.array ):
-    b = np.empty_like(a)
+    b = np.empty_like(data,dtype= int)
     for i in range(data.shape[0]):
        for j in range(data.shape[1]):
-         b[i][j][0] = (data[i][j][0] + 2*data[i][j][1] + data[i][j][2])/4          
-         b[i][j][1] = data[i][j][2] - data[i][j][1]
-         b[i][j][2] = data[i][j][0] - data[i][j][1]
+         b[i][j][0] = (int(data[i][j][0]) + 2*int(data[i][j][1]) + int(data[i][j][2]))/4          
+         b[i][j][1] = int(data[i][j][2]) - int(data[i][j][1])
+         b[i][j][2] = int(data[i][j][0]) - int(data[i][j][1])
     return b
 
 """
@@ -36,12 +36,17 @@ def FromYUV(data:np.array ):
 
 
 def KL(data:np.array):
+    # Moyenne
+    pixels = 0
     Moy = np.array([0.0,0.0,0.0])
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
            Moy += data[i][j]
-    Moy/=(data.shape[0]*data.shape[1])
+           pixels += 1
+    Moy /= pixels
     print(Moy)
+
+    # Covariance
     """Github du Cours"""
     covRGB = np.zeros((3,3), dtype = "double")
     for i in range(data.shape[0]):
@@ -49,16 +54,18 @@ def KL(data:np.array):
           vecTemp=[[data[i][j][0] - Moy[0]], [data[i][j][1] - Moy[1]], [data[i][j][2] - Moy[2]] ]
           vecProdTemp = np.dot(vecTemp,np.transpose(vecTemp))
           covRGB = np.add(covRGB,vecProdTemp)
-    covRGB /=(data.shape[0]*data.shape[1])
+    covRGB /= pixels
     print(covRGB)
+
+    # Vecteur et valeur propres
     eigval, eigvec = LA.eig(covRGB)
     print(eigval)
     print(eigvec)
-
     # 
     eigvec = np.transpose(eigvec)
+    print(eigvec)
 
-    imageKL = np.copy(data)
+    imageKL = np.empty_like(data)
 
     vecMoy =[[Moy[0]], [Moy[1]], [Moy[2]]] 
 
@@ -70,32 +77,98 @@ def KL(data:np.array):
         
     return (imageKL, eigvec, Moy)
 
+def KLInverse(data:np.array, vector:np.array, Moy:np.array):
+    inv = LA.pinv(vector)
+    vecMoy =[[Moy[0]], [Moy[1]], [Moy[2]]] 
+    imageOri = np.empty_like(data)
+
+    for i in range(data.shape[0]):
+      for j in range(data.shape[1]):
+        vecTemp=[[data[i][j][0] - Moy[0]], [data[i][j][1] - Moy[1]], [data[i][j][2] - Moy[2]] ]
+        imageOri[i][j][:] = np.reshape(np.dot(inv,np.subtract(vecTemp,vecMoy)),(3))
+    return imageOri
 
 """
-suppose entrée et sortie sur 8 bits, entree devient sur values bits
+suppose entrée sur 8 bits et sorties sur values bits
 """
 def Quantify(data:np.array, values:tuple[int,int,int]):
     values = [8-i for i in values]
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             data[i][j][0] = data[i][j][0] >> values[0]  
-            data[i][j][0] = data[i][j][0] >> values[1]
-            data[i][j][0] = data[i][j][0] >> values[2]
+            data[i][j][1] = data[i][j][1] >> values[1]
+            data[i][j][2] = data[i][j][2] >> values[2]
 
 """
-suppose entrée et sortie sur 8 bits, entree devient 
+suppose entrées sur values bit et sortie sur 8 bits 
 """
 def Unquantify(data:np.array, values:tuple[int,int,int]):
     values = [8-i for i in values]
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             data[i][j][0] = data[i][j][0] << values[0]  
-            data[i][j][0] = data[i][j][0] << values[1]
-            data[i][j][0] = data[i][j][0] << values[2]
+            data[i][j][1] = data[i][j][1] << values[1]
+            data[i][j][2] = data[i][j][2] << values[2]
 
-with Image.open(r"TP2\\data\\kodim0"+str(i) +".png") as im:
-  red = list(im.getdata(0))
-  green = list(im.getdata(1))
-  blue = list(im.getdata(2))
-  array = np.asarray(im)    
-  imageKL, vecteur, Moyeenne = KL(array)
+with Image.open(r"TP2\\data\\kodim01.png") as im:
+  """  array = np.asarray(im) 
+  imageYUV = ToYUV(array)
+  imageKL, vecteur, Moyenne = KL(imageYUV)
+  original = KLInverse(imageKL,vecteur,Moyenne)
+  print(imageYUV[2][4])
+  print(imageKL[2][4])
+  print(original[2][4])
+  """
+  
+  import matplotlib.pyplot as py
+  fig1 = py.figure(figsize = (10,10))
+  imagelue = py.imread(r"TP2\\data\\kodim01.png")
+  image=imagelue.astype('double')
+
+  Moyenne = np.array([0.0,0.0,0.0])
+
+  for i in range(len(image)):
+      for j in range(len(image[0])):
+          Moyenne[0]+= image[i][j][0]
+          Moyenne[0]+= image[i][j][1]
+          Moyenne[0]+= image[i][j][2]
+          
+  nbPixels = len(image)*len(image[0])        
+
+  Moyenne[0] /= nbPixels
+  Moyenne[1] /= nbPixels
+  Moyenne[2] /= nbPixels
+
+  covRGB = np.zeros((3,3), dtype = "double")
+  for i in range(len(image)):
+      for j in range(len(image[0])):
+          vecTemp=[[image[i][j][0] - Moyenne[0]], [image[i][j][1]] - Moyenne[1], [image[i][j][2] - Moyenne[2]]]
+          vecProdTemp = np.dot(vecTemp,np.transpose(vecTemp))
+          covRGB = np.add(covRGB,vecProdTemp)
+
+  covRGB = covRGB / nbPixels        
+
+  eigval, eigvec = LA.eig(covRGB)
+
+  eigvec = np.transpose(eigvec)
+  imageKL = np.copy(image)  
+
+  vecMoy =[[Moyenne[0]], [Moyenne[1]], [Moyenne[2]]] 
+
+  for i in range(len(image)):
+      for j in range(len(image[0])):
+          vecTemp=[[image[i][j][0]], [image[i][j][1]], [image[i][j][2]]]
+          #a=Mb
+          imageKL[i][j][:] = np.reshape(np.dot(eigvec,np.subtract(vecTemp,vecMoy)),(3))
+
+  invEigvec = LA.pinv(eigvec)
+
+  vecMoy =[Moyenne[0], Moyenne[1], Moyenne[2]] 
+  imageRGB = np.copy(image)
+
+  for i in range(len(image)):
+      for j in range(len(image[0])):
+        #b=inv(M)a
+            vecTemp=[[imageKL[i][j][0]], [imageKL[i][j][1]], [imageKL[i][j][2]]]
+            imageRGB[i][j][:] = np.add(np.reshape(np.dot(invEigvec,vecTemp),(3)),vecMoy)
+            print(str(image[i][j]) + " "+ str(imageRGB[i][j]) )
