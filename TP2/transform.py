928d378535d655b4dfd3a269e458d807bb05786d
Mathias Gagnon <mathias.gagnon@polymtl.ca>
2024-02-17 16:20:08 -0500

    focntionnel

--- TP2/transform.py
+++ TP2/transform.py
@@ -2,18 +2,11 @@ from PIL import Image
 import cv2
 import numpy as np
 from numpy import linalg as LA
-
+import math
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
-
-# https://gist.github.com/nimpy/5b0085075a54ba2e94f2cfabf5a98a57
-def calculate_psnr(img1, img2, max_value=255):
-    """"Calculating peak signal-to-noise ratio (PSNR) between two images."""
-    mse = np.mean((np.array(img1, dtype=np.float32) - np.array(img2, dtype=np.float32)) ** 2)
-    if mse == 0:
-        return 100
-    return 20 * np.log10(max_value / (np.sqrt(mse)))
-
+import copy
+0
 """
   Y = (R + 2G + B) /4
   U = B − G
@@ -34,7 +27,7 @@ def ToYUV(data:np.array ):
   B = U + G 
 """
 def FromYUV(data:np.array ):
-    b = np.copy(data).astype('double')
+    b = copy.deepcopy(data).astype('double')
     for i in range(data.shape[0]):
        for j in range(data.shape[1]):
          b[i][j][1] = data[i][j][0] - (data[i][j][1] + data[i][j][2])/4
@@ -88,26 +81,25 @@ def KLInverse(image:np.array, eigvec:np.array, Moyenne:np.array):
   invEigvec = LA.pinv(eigvec)
 
   vecMoy =[Moyenne[0], Moyenne[1], Moyenne[2]] 
-  imageTR = np.copy(image)
+  imageTR = copy.deepcopy(np.copy(image))
 
   for i in range(len(image)):
       for j in range(len(image[0])):
         #b=inv(M)a
-            vecTemp=[[imageKL[i][j][0]], [imageKL[i][j][1]], [imageKL[i][j][2]]]
+            vecTemp=[[image[i][j][0]], [image[i][j][1]], [image[i][j][2]]]
             imageTR[i][j][:] = np.add(np.reshape(np.dot(invEigvec,vecTemp),(3)),vecMoy)
-  return imageTR
+  return imageTR.astype("int8")
 
 """
 suppose entrée sur 8 bits et sorties sur values bits
 """
 def Quantify(data:np.array, values:tuple[int,int,int]):
     values_usable = [8-i for i in values]
-    dataNew = np.copy(data).astype("int8")
+    dataNew = np.copy(data).astype("int16")
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             for channel in range(data.shape[2]):                
               dataNew[i][j][channel] = ( dataNew[i][j][channel] >> values_usable[channel] ) << values_usable[channel]
-    print("Quantification:")
     return dataNew 
 
 liste_images = [r"TP2\\data\\kodim01.png",r"TP2\\data\\kodim02.png",r"TP2\\data\\kodim05.png",r"TP2\\data\\kodim13.png",r"TP2\\data\\kodim23.png"]
@@ -124,20 +116,24 @@ for Pathimage in liste_images:
     """
     
     import matplotlib.pyplot as py  
-    fig1 = py.figure(figsize = (10,10))
     imagelue = np.asarray(im)
+    Original = copy.deepcopy(imagelue)
 
     imageC=imagelue.astype('double')
     image = ToYUV(imageC)
 
     imageKL, eigvec, Moyenne = KL(image)
 
-    Quantifications = [(8,8,8),(8,8,4) ,(8,8,0),(8,6,2)]
+    Quantifications = [(8,8,8),(8,8,4),(8,8,0),(8,6,2)]
+
     for Values in Quantifications:
+        c = copy.deepcopy(imageKL)
         print("Compression:\t", Values)
-        quantified = Quantify(imageKL,Values)
+        quantified = Quantify(c,Values)
 
         inverse = KLInverse(quantified,eigvec,Moyenne)
-        rgb = FromYUV(inverse)
 
-    break
+        rgb = FromYUV(inverse)
+        print("\tPSNR:\t", psnr(imagelue,rgb))
+        print("\tSSIM:\t",ssim(imagelue,rgb,channel_axis=2))
+        
