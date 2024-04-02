df9add5cc5da2a395e04b4c7670a20a71dd4c677
Mathias Gagnon <mathias.gagnon@polymtl.ca>
2024-04-02 18:42:04 -0400

    Q2,3

--- TP3/Calculs.py
+++ TP3/Calculs.py
@@ -10,7 +10,7 @@ import csv
 def CalcSimCos(base:np.ndarray, ref:np.ndarray):
   local_b = base.flatten()
   local_r = ref.flatten()
-  return 1 - (np.dot(local_b,local_r)/(norm(local_r)*norm(local_b)))
+  return (np.dot(local_b,local_r)/(norm(local_r)*norm(local_b)))
 
 def CalcDist(base:np.ndarray, ref:np.ndarray):
   local_b = base.flatten()/base.sum()
@@ -18,22 +18,16 @@ def CalcDist(base:np.ndarray, ref:np.ndarray):
   dist:np.ndarray = np.absolute([local_b - local_r])
   return dist.sum()
 
-def CalculHisto(image: np.ndarray,taille):
+def CalculHisto3D(image: np.ndarray): ## Taille 64
   image.astype('uint8')
-  flat = image.flatten()
-  hist = cv2.calcHist([flat], [0], None, [256//taille], [0, 256])
-  image -= taille//2
-  hist2 = cv2.calcHist([flat], [0], None, [256//taille], [0, 256])
-  return [hist,hist2]
+  hist:np.ndarray = cv2.calcHist([image], [0,1,2], None, [4,4,4], [0, 256,0, 256,0, 256],).astype("uint32")
+  image -= 32
+  hist2 = cv2.calcHist([image], [0,1,2], None, [4,4,4], [0, 256,0, 256,0, 256]).astype("uint32")
+  concatenated:np.ndarray =  np.concatenate([hist,hist2])
+  t = np.array([concatenated & 0x0000FFFF,concatenated & 0x00FF00FF,concatenated & 0x00FFFF00],np.uint8).flatten()
+  return concatenated
 
-def CalculHisto3D(image: np.ndarray, taille):
-  image.astype('uint8')
-  hist = cv2.calcHist([image], [0,1,2], None, [256//taille,256//taille,256//taille], [0, 256,0, 256,0, 256])
-  image -= taille//2
-  hist2 = cv2.calcHist([image], [0,1,2], None, [256//taille,256//taille,256//taille], [0, 256,0, 256,0, 256])
-  return [hist,hist2]
-
-def extract_frames(video_path,histogramCalculation, taille):
+def extract_frames1s(video_path, couleur):
     # Open the video file
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
@@ -49,8 +43,36 @@ def extract_frames(video_path,histogramCalculation, taille):
             break
 
         # Convert frame to numpy array and store it
-        frame_np =cv2.cvtColor(np.asarray(frame),cv2.COLOR_BGR2YUV) 
-        frames.append((histogramCalculation(frame_np,taille),frame_count/3))
+        frame_np =couleur(np.asarray(frame)) 
+        frames.append((CalculHisto3D(frame_np),frame_count))
+
+        frame_count += 1
+
+        # Skip frames to keep 3 frame per second
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_count * cap.get(cv2.CAP_PROP_FPS))
+
+    cap.release()
+
+    return (video_path.split('\\')[-1][:4],frames)
+
+def extract_frames3ps(video_path, couleur):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Could not open video file")
+        return None
+
+    frames = []
+    frame_count = 0
+    # Read and process each frame
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert frame to numpy array and store it
+        frame_np =couleur(np.asarray(frame)) 
+        frames.append((CalculHisto3D(frame_np),frame_count/3))
 
         frame_count += 1
 
@@ -61,79 +83,76 @@ def extract_frames(video_path,histogramCalculation, taille):
 
     return (video_path.split('\\')[-1][:4],frames)
 
+def GetBestCorr(liste):
+  best = liste[0]
+  for i in range(1,len(liste)):
+    if liste[i][1] < best[1]:
+        best = liste[i]
+  return best
+
 if __name__ == '__main__':
   import time
   now = time.time()
 
-  fonctions_histo = [lambda x,y: CalculHisto(x,y)[0],lambda x,y: np.concatenate(CalculHisto(x,y)), lambda x,y: CalculHisto3D(x,y)[0],lambda x,y: np.concatenate(CalculHisto3D(x,y))]
-  fonctions_dist = [CalcSimCos, CalcDist]
+  # Espace de couleur avec leur seuil
+  fonctions_color = [lambda x: cv2.cvtColor(x,cv2.COLOR_BGR2YUV), lambda x: x]
+  seuils = [0.6,0.42]
+  desc_color = ["YUV", "RGB"]
+  # Exctraction des videos
+  fonctions_temps = [extract_frames3ps,extract_frames1s]
+  desc_temps = ["3ps","1ps"]
+  for c in range(len(fonctions_color)):
+    f_color = fonctions_color[c]
 
-  for h in range(len(fonctions_histo)):
-    f_hist = fonctions_histo[h]
+    for f in range(len(fonctions_temps)):
+      f_extract = fonctions_temps[f]
 
-    for d in range(len(fonctions_dist)):
-      f_dist = fonctions_dist[d]
+      # 
+      if c == 0 and f == 0:
+         continue
 
-      for t in range(5,7):
-        captures = []
-        taille = 2**t
-        now = time.time()
+      captures = []
+      
+      now = time.time()
 
-        for target in range(1,101):
-          video_path = "TP3\moodle\data\mp4\\v" + str(target).zfill(3) +  ".mp4"
-          captures.append(extract_frames(video_path,f_hist,taille))
+      for target in range(1,101):
+        video_path = "TP3\moodle\data\mp4\\v" + str(target).zfill(3) +  ".mp4"
+        captures.append(f_extract(video_path,f_color))
 
-        temps_indexation = time.time() - now
+      temps_indexation = time.time() - now
 
-        print("Durée indexation:")
-        print(temps_indexation)
+      print("Durée indexation:")
+      print(temps_indexation)
 
-        def GetBestCorr(liste):
-          best = liste[0]
-          for i in range(1,len(liste)):
-            if liste[i][1] < best[1]:
-                best = liste[i]
-          return best
+      csv_file_path = "results_"+desc_temps[f]+"s_Coul"+desc_color[c]+"_hist3d_2pieces_taille64_distManh.csv"
 
-        csv_file_path = "results_histYUV"+ str(h)+ "_taille" + str(t) + "_dist" + str(d) + ".csv"
+      now = time.time()
+      with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(("image","video_pred","minutage_pred"))
 
-        now = time.time()
-        with open(csv_file_path, 'w', newline='') as csvfile:
-          with open("TP3\moodle\data\gt.csv", 'r', newline='') as TruthFile:
-            csv_writer = csv.writer(csvfile)
-            headers = TruthFile.readline()
-            bad = 0
-            far = 0
-            for i in range(0,1000):
-              image, reponse, minutage = TruthFile.readline().split(',')
+        for i in range(0,1000):
+          image_path = "TP3\moodle\data\jpeg\i" + str(i).zfill(3) +".jpeg"
+          histogram = CalculHisto3D(f_color(cv2.imread(image_path)))
+          res = []
+          for video in captures:
+            rows_b = []
+            rows_m = []
+            for frame in video[1]:
+              dist = CalcDist(histogram,frame[0])
+              if dist < seuils[c]:
+                rows_b.append((frame[1],dist))
 
-              image_path = "TP3\moodle\data\jpeg\i" + str(i).zfill(3) +".jpeg"
-              histogram = f_hist(cv2.cvtColor(cv2.imread(image_path),cv2.COLOR_BGR2YUV),taille)
-              res = []
-              for video in captures:
-                rows_b = []
-                rows_m = []
-                for frame in video[1]:
-                  dist = f_dist(histogram,frame[0])
-                  if dist < 0.35:
-                    rows_b.append((frame[1],dist))
+            if len(rows_b) > 0:
+              b = GetBestCorr(rows_b)
+              res.append((video[0],b[0],b[1]))
+  
+          final = None 
+          if len(res) == 0:
+            final = ("out","",1)
+          else: 
+            res.sort(key= lambda x: x[2])  
+            final = res[0]
 
-                
-                if len(rows_b) > 0:
-                  b = GetBestCorr(rows_b)
-                  res.append((video[0],b[0],b[1]))
-              final = None 
-              if len(res) == 0:
-                final = ("out","",1)
-              else: 
-                res.sort(key= lambda x: x[2])  
-                final = res[0]
-              if final[0] != reponse:
-                csv_writer.writerow(("i"+str(i).zfill(3), final[0],final[1],final[2], "Bad" + reponse))
-                bad += 1
-              elif final[2] > 0.25 and final[0] != "out":
-                csv_writer.writerow(("i"+str(i).zfill(3), final[0],final[1],final[2], "Far"))
-                far += 1
-
-            csv_writer.writerow(("Bad", bad,"Far",far))
-          csv_writer.writerow(("indexation", temps_indexation,"Tests",time.time() - now))
+          csv_writer.writerow(("i"+str(i).zfill(3), final[0],final[1]))
+        csv_writer.writerow(("indexation", temps_indexation,"Tests",time.time() - now))
